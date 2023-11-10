8d83cfaa29b1b132cd33e0b1349f256359ddf199
tmolteno <tim@molteno.net>
2023-11-11 02:52:55 +1300

    Update cosign version

--- test.sh
+++ test.sh
@@ -8,11 +8,12 @@ TART_CAL_ELEVATION=55
 TART_CAL_ITERATIONS=800
 TART_CAL_POINTING=0
 
-rm -rf ${OUTPUT_DIR}
-mkdir -p ${DATA_DIR}
-rm -rf ${DATA_DIR}/*
-python3 raw_cal/get_cal_data.py --api ${TART_API} --pw sharkbait --n ${TART_NCAL} --interval ${TART_CAL_INT} --dir ${DATA_DIR}
+
+#rm -rf ${OUTPUT_DIR}
+#mkdir -p ${DATA_DIR}
+#rm -rf ${DATA_DIR}/*
+#python3 raw_cal/get_cal_data.py --api ${TART_API} --pw sharkbait --n ${TART_NCAL} --interval ${TART_CAL_INT} --dir ${DATA_DIR}
 
 # Perform optimization
 # python3 raw_cal/pos_from_gps.py --api ${TART_API}  --elevation ${TART_CAL_ELEVATION}  --data ${DATA_DIR}  --dir ${OUTPUT_DIR}
-python3 raw_cal/tart_cal.py --api ${TART_API} --phases --iterations ${TART_CAL_ITERATIONS} --pointing ${TART_CAL_POINTING} --elevation ${TART_CAL_ELEVATION}  --data ${DATA_DIR}  --dir ${OUTPUT_DIR}
+python3 raw_cal/tart_cal.py --api ${TART_API}  --iterations ${TART_CAL_ITERATIONS} --pointing ${TART_CAL_POINTING} --elevation ${TART_CAL_ELEVATION}  --data ${DATA_DIR}  --dir ${OUTPUT_DIR}
