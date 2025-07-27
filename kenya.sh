8f1fd9b5a6c3563ef6d2a3e8d0077d8594431bdb
Tim Molteno <tim@elec.ac.nz>
2025-07-27 16:02:39 +1200

    Close figures after we are done

--- kenya.sh
+++ kenya.sh
@@ -2,14 +2,14 @@
 while [ 0 ]
 do
         TARGET=tart-kenya \
-        TART_CAL_ARGS="" \
+        TART_CAL_ARGS="--get-gains" \
         TART_GET_DATA=1 \
-        TART_NCAL=2 \
-        TART_CAL_INT=7 \
+        TART_NCAL=3 \
+        TART_CAL_INT=11 \
         TART_CAL_ITERATIONS=800 \
         TART_CAL_ELEVATION=45 \
         TART_CAL_POINTING=0 \
-        TART_CAL_POINTING_RANGE=2.5 \
+        TART_CAL_POINTING_RANGE=1.5 \
         TART_CAL_UPLOAD=1 \
         TART_LOGIN_PW=nairobi ./do_raw_cal.sh
 done
